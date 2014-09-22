68a8fe586634d5ff613c3657917e75d7c5cae144
Daniel Luxemburg <daniel.luxemburg@gmail.com>
2014-09-21 21:21:53 -0400

    gracefully handle no beacon scenario on refresh, shut off blink1 when powering down

--- clear-display.js
+++ clear-display.js
@@ -1,9 +1,19 @@
-var Display = require('./lib/display').Display
-var display = new Display()
+var Display = require('./lib/display').Display,
+    Blink1 = require('node-blink1')
+
+var display = new Display(),
+    blink1 = new Blink1()
+
 setTimeout(function(){
-  display.clear()
-  display.setBacklight(false)
+  try{
+    display.clear()
+    display.setBacklight(false)
+  }catch(e){}
+  try{
+    blink1.setRGB(0, 0, 0, function(){})
+  }catch(e){}
 }, 250)
+
 setTimeout(function(){
   process.exit()
 }, 500)
